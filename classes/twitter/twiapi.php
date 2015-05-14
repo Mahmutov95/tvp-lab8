b3d0a9af6d40a4e39b5fe2d7d9031ade5f436a58
DolgikhDenis <den-dolgih@mail.ru>
2015-05-14 21:55:55 +0500

    исправлено

--- classes/twitter/twiapi.php
+++ classes/twitter/twiapi.php
@@ -6,11 +6,49 @@
 	define(CONSUMER_KEY, 'Jky813XwbmOXCiE3barLsq5QT');
 	define(CONSUMER_SECRET, 'AtgwEFXAblqEccb3WlKM1tINz8aqF8TZP3T2UZ0kH9UQBhCDDY');
 	define(OAUTH_CALLBACK, 'oob');
+	
+	
+	define("CONSUMER_KEY_1", "OK62r0UsSIZgcReixxgjQG9bj");
+	define("CONSUMER_SECRET_1", "YwmvamiM2sHmrGIwvcOr6Ls6vXYUVh3fJFLUpnCKdwuYYsGBj3");
+	define("OAUTH_TOKEN", "2362321468-U90WvkudGKmmdvFMDBXtYar6pIDVmwj4i2TkmlL");
+	define("OAUTH_SECRET", "dXfTr2IVy2wX7d6kWwqRS2SOUY2WaNqmofg36V9VrMWA5");
+	$count = 10;
 
 	class TwiApi
 	{
 		public $connection;
 
+		
+		function SearchByHashTags($ht)
+		{
+			$this->connection = new TwitterOAuth(CONSUMER_KEY_1, CONSUMER_SECRET_1, OAUTH_TOKEN, OAUTH_SECRET);
+			$statuses = $this->connection ->get("search/tweets", array("q" => "#".$ht, "count" => $count));
+			foreach($statuses as $twit){
+				foreach($twit as $tweet){
+					echo '<tr>';
+					echo '<td> <img src="'.$tweet->user->profile_image_url.'" /> </td>';
+					echo "<td>".$tweet->user->name."</td>";
+					echo "<td>".preg_replace("/({$ht})/iu", '<strong>$1</strong>', $tweet->text)."</td>";
+					echo '</tr>';
+				}
+			}
+		}
+		
+		function SearchByKeyWord($word)
+		{
+			$this->connection = new TwitterOAuth(CONSUMER_KEY_1, CONSUMER_SECRET_1, OAUTH_TOKEN, OAUTH_SECRET);
+			$statuses = $this->connection ->get("search/tweets", array("q" => $word, "count" => $count));
+			foreach($statuses as $twit){
+				foreach($twit as $tweet){
+					echo '<tr>';
+					echo '<td> <img src="'.$tweet->user->profile_image_url.'" /> </td>';
+					echo "<td>".$tweet->user->name."</td>";
+					echo "<td>".preg_replace("/({$word})/iu", '<strong>$1</strong>', $tweet->text)."</td>";
+					echo '</tr>';
+				}
+			}
+		}
+		
 		function __construct()
 		{
 			session_start();
@@ -56,32 +94,6 @@
 			$correct = $response->users;
 			$result = $correct;
 			return $result;
-		}
-		
-		function FindTweetsbyHashtag ($hash)
-		{
-			$tweetss = $this->connection->get("search/tweets", array("q" => "#"$hash, "result_type"=>"recent", "count"=>"50"));
-			foreach($tweetss as $tweet){
-				foreach($tweet as $t){
-					echo '<tr>';
-					echo '<td> <img src="'.$t->user->profile_image_url.'" /> </td>';
-					echo "<td>".$t->text."</td>";
-					echo '</tr>';
-				}
-			}
-		}
-		
-		function FindTweetsbyKeyword ($keyword)
-		{
-		$tweetss = $this->connection->get("search/tweets", array("q" => $keyword, "result_type"=>"recent", "lang"=>"ru", "count"=>"50"));
-			foreach($tweetss as $tweet){
-				foreach($tweet as $t){
-					echo '<tr>';
-					echo '<td> <img src="'.$t->user->profile_image_url.'" /> </td>';
-					echo "<td>".preg_replace("/({$keyword})/iu", '<strong>$1</strong>', $t->text)."</td>";
-					echo '</tr>';
-				}
-			}
 		}
 	}
 ?>
